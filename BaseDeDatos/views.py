093190d42e3611538f0793ea6221db02cb14cd65
Alfonso Casanoves <casanovesdavid@gmail.com>
2022-10-23 16:49:57 -0300

    Acercadenoscambios

--- BaseDeDatos/views.py
+++ BaseDeDatos/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required
+
 def inicio(request):
     return render(request, "inicio.html")
 @login_required
